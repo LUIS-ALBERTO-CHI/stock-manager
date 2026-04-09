73661963f158a0a6093b166d593b314cb768858d
Luis A. Casanova <132479713+LuisCasanova11@users.noreply.github.com>
2026-04-08 18:59:10 -0600

    ENABLE

--- api/movements.js
+++ api/movements.js
@@ -2,49 +2,49 @@ import { sql } from "@vercel/postgres";
 
 export default async function handler(req, res) {
 
-//   if (req.method === "GET") {
+  if (req.method === "GET") {
 
-//     const area = req.query.area;
+    const area = req.query.area;
 
-//     const { rows } = await sql`
-//       SELECT * FROM movements
-//       WHERE area = ${area}
-//       ORDER BY id DESC
-//     `;
+    const { rows } = await sql`
+      SELECT * FROM movements
+      WHERE area = ${area}
+      ORDER BY id DESC
+    `;
 
-//     return res.json(rows);
-//   }
+    return res.json(rows);
+  }
 
-//   if (req.method === "POST") {
+  if (req.method === "POST") {
 
-//     const { product, qty, type, area } = req.body;
+    const { product, qty, type, area } = req.body;
 
-//     await sql`
-//       INSERT INTO movements (product, qty, type, area)
-//       VALUES (${product}, ${qty}, ${type}, ${area})
-//     `;
+    await sql`
+      INSERT INTO movements (product, qty, type, area)
+      VALUES (${product}, ${qty}, ${type}, ${area})
+    `;
 
-//     if(type === "entrada"){
+    if(type === "entrada"){
 
-//       await sql`
-//         UPDATE products
-//         SET stock = stock + ${qty}
-//         WHERE name = ${product}
-//         AND area = ${area}
-//       `;
+      await sql`
+        UPDATE products
+        SET stock = stock + ${qty}
+        WHERE name = ${product}
+        AND area = ${area}
+      `;
 
-//     } else {
+    } else {
 
-//       await sql`
-//         UPDATE products
-//         SET stock = stock - ${qty}
-//         WHERE name = ${product}
-//         AND area = ${area}
-//       `;
+      await sql`
+        UPDATE products
+        SET stock = stock - ${qty}
+        WHERE name = ${product}
+        AND area = ${area}
+      `;
 
-//     }
+    }
 
-//     return res.json({ ok:true });
-//   }
+    return res.json({ ok:true });
+  }
 
 }
